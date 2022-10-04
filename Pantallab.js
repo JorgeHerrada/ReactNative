f8eb53940acd18761dadb7f58841c65a0d186337
JorgeHerrada <jorge.herrada.28@gmail.com>
2022-10-04 00:42:12 -0500

    Styled pantallab

--- Pantallab.js
+++ Pantallab.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default class Pantallab extends Component {
@@ -30,28 +30,69 @@ export default class Pantallab extends Component {
     // creacion de la celda que despliega los datos
     const celda = ({item}) => {
       return(
-        <View style={{margin:10,borderWidth:1,borderColor:"black",flex:1,marginBottom:50,marginTop:10}}>
-          <Text>id: {item.id}</Text>
-          <Text>nombre: {item.nombre}</Text>
-          <Text>codigo: {item.codigo}</Text>
-          <Text>tarea: {item.tarea}</Text>
+        <View style={styles.celdaContainer}>
+          <Text style={styles.celda}>id: {item.id}</Text>
+          <Text style={styles.celda}>nombre: {item.nombre}</Text>
+          <Text style={styles.celda}>codigo: {item.codigo}</Text>
+          <Text style={styles.celda}>tarea: {item.tarea}</Text>
         </View>
       )
     }
 
     return (
-      <View>
-        <Text> Bienvenido: {this.props.route.params.nombre} </Text>
-        <Text> Código: {this.props.route.params.codigo} </Text>
+      <View style={styles.background}>
+        <View style={styles.infoContainer}>
+          <Text style={styles.info}>Bienvenido: {this.props.route.params.nombre} </Text>
+          <Text style={styles.info}>Código: {this.props.route.params.codigo} </Text>
+        </View>
 
         {/* Creates list and display all data received from server */}
         <FlatList
           data={this.state.datosServer}
           renderItem={celda}
           keyExtractor={(item,index) => index.toString()}
+          style={styles.flatList}
         />
 
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  background:{
+    backgroundColor:"#FFD1D1",
+    flex: 1,  
+    opacity: 1
+  },
+  celdaContainer:{
+    marginHorizontal:20,
+    marginVertical:20,
+    borderWidth:2,
+    borderColor:"black",
+    padding:10,
+    borderRadius:15,
+    flex:1,
+    backgroundColor: "#FFE3E1",
+  },
+  celda:{
+    fontSize:20,
+    fontFamily:"serif",
+  },
+  infoContainer:{
+    padding:20,
+    borderBottomWidth:2,
+    backgroundColor: "#FF9494",
+    alignContent:"center",
+  },
+  info:{
+    fontFamily:"serif",  
+    fontSize:15,  
+    fontWeight:"bold",
+    alignSelf:"center",
+  },
+  flatList:{
+    flex: 1,
+    marginBottom: 30,
+  },
+})
